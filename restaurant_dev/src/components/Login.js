7eda28f3960cfbd615bd5f75cd777bfc324407b0
Pit Fröhlich <pitfroehlich@hotmail.de>
2021-11-10 19:25:43 +0100

    Registrierung von Usern

--- restaurant_dev/src/components/Login.js
+++ restaurant_dev/src/components/Login.js
@@ -1,19 +1,68 @@
 import React from 'react'
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button'
-
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './firebase_config';
 const Login = () => {
+
+    const [registerEmail, setRegisterEmail] = useState('');
+    const [registerPassword, setRegisterPassword] = useState('');
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+
+    const register = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(
+                auth,
+                registerEmail,
+                registerPassword
+            );
+            console.log(user);
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    }
+
+    const login = async () => {
+
+    }
+
+    const logOut = async () => {
+
+    }
+
     return (
         <div>
-           <h1>Register</h1>
-           <input type="text" placeholder="E-Mail" />
-           <input type="text" placeholder="Password" />
-           <Button variant="primary">Register</Button> 
-           <h1>Login</h1>
-           <input type="text" placeholder="E-Mail" />
-           <input type="text" placeholder="Password" />
-           <Button variant="primary">Login</Button>
-           <h1>User logged in:</h1>
-           <Button>Sign out</Button>
+            <h1>Register</h1>
+            <input type="text"
+                placeholder="E-Mail"
+                onChange={(event) => {
+                    setRegisterEmail(event.target.value);
+                }} />
+
+            <input type="text"
+                placeholder="Password"
+                onChange={(event) => {
+                    setRegisterPassword(event.target.value);
+                }}
+            />
+            <Button variant="primary" onClick={register}>Register</Button>
+            <h1>Login</h1>
+            <input type="text"
+                placeholder="E-Mail"
+                onChange={(event) => {
+                    setLoginEmail(event.target.value);
+                }} />
+            <input type="text"
+                placeholder="Password"
+                placeholder="Password"
+                onChange={(event) => {
+                    setLoginPassword(event.target.value);
+                }} />
+            <Button variant="primary">Login</Button>
+            <h1>User logged in:</h1>
+            <Button>Sign out</Button>
         </div>
     )
 }
